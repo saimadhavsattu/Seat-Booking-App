6a9b1f9283ff696701af89914ee327325a5b14a6
saimadhavsattu <68007466+saimadhavsattu@users.noreply.github.com>
2024-10-07 16:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//Create you project here from scratch
+const moviesList = [
+    { movieName: "Flash", price: 7 },
+    { movieName: "Spiderman", price: 5 },
+    { movieName: "Batman", price: 4 },
+  ];
+// Use moviesList array for displaing the Name in the dropdown menu
+const selectElement = document.querySelector(".selectMovie");
+
+//Add eventLister to each unoccupied seat
+//Add eventLsiter to continue Button
+//Add eventListerner to Cancel Button
